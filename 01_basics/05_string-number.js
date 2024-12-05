767bc7e34e741808807fe12f7c7577741e010669
Prashant Choudhary <prashantchoudhary13579@gmail.com>
2024-12-05 21:06:39 +0000

    learning about operations

---
+++ 01_basics/05_string-number.js
@@ -0,0 +1,30 @@
+// operations
+let value = 3;
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "Prashant ";
+let str2 = "Choudhary";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2 + 2);
+// console.log("1" + "2");
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+// it is bad to write the tricky conversions if you want to write something so write in such way that another person can easily understand it.
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);

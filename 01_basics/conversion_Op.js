7b978403d62d859e65efef6081107d53a35afd3a
Prashant Choudhary <prashantchoudhary13579@gmail.com>
2024-12-05 19:43:13 +0000

    learning the concept of data conversion

---
+++ 01_basics/conversion_Op.js
@@ -0,0 +1,14 @@
+let score = undefined
+
+// console.log(typeof score);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33"=> 33
+// "33abc"  =>  NaN (not a number)
+// true => 1; false => 0
+
+let isloggedin = 1
+let booleanIsLoggedIn = Boolean(isloggedin)

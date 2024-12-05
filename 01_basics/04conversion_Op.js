5c56661f5dae704b0e0885df5b098c389bdd0e78
Prashant Choudhary <prashantchoudhary13579@gmail.com>
2024-12-05 20:16:42 +0000

    Renaming

---
+++ 01_basics/04conversion_Op.js
@@ -0,0 +1,30 @@
+let score = undefined
+
+// console.log(typeof score);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33"=> 33
+// "33abc"  =>  NaN (not a number)
+// true => 1; false => 0
+
+let isloggedin = "-44"
+let booleanIsLoggedIn = Boolean(isloggedin)
+// console.log(isloggedin);
+// console.log(booleanIsLoggedIn);
+
+// "1"- true
+// "0"- false
+// "prashant" - true
+// null - false
+// undefine - false
+// "45" - true
+// any value except 0 in " " -> true
+// Else it will give u -> false
+
+let someNumber = null
+let stringNum = String(someNumber)
+console.log(stringNum)
+console.log(typeof stringNum)
